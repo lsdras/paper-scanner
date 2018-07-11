2cabeffa50fcb7c335d0ef2825dc473dae0526e2
lsdras <lsdras96@gmail.com>
2018-07-11 23:12:54 +0900

    acquiring the mask rcnn input modes

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -102,18 +102,20 @@ class Config(object):
 
     # Input image resizing
     # Generally, use the "square" resizing mode for training and inferencing
-    # and it should work well in most cases. In this mode, images are scaled
-    # up such that the small side is = IMAGE_MIN_DIM, but ensuring that the
-    # scaling doesn't make the long side > IMAGE_MAX_DIM. Then the image is
-    # padded with zeros to make it a square so multiple images can be put
-    # in one batch.
+    # and it should work well in most cases. In this mode,
+    # images are scaled up such that the small side is = IMAGE_MIN_DIM,
+    # but ensuring that the scaling doesn't make the long side > IMAGE_MAX_DIM.
+    # Then the image is padded with zeros to make it a square
+    # so multiple images can be put in one batch.
+
     # Available resizing modes:
     # none:   No resizing or padding. Return the image unchanged.
     # square: Resize and pad with zeros to get a square image
     #         of size [max_dim, max_dim].
     # pad64:  Pads width and height with zeros to make them multiples of 64.
     #         If IMAGE_MIN_DIM or IMAGE_MIN_SCALE are not None, then it scales
-    #         up before padding. IMAGE_MAX_DIM is ignored in this mode.
+    #         up before padding.
+    #         IMAGE_MAX_DIM is ignored in this mode.
     #         The multiple of 64 is needed to ensure smooth scaling of feature
     #         maps up and down the 6 levels of the FPN pyramid (2**6=64).
     # crop:   Picks random crops from the image. First, scales the image based
--- note_scan_main.py
+++ note_scan_main.py
@@ -18,7 +18,7 @@ ROOT_DIR = os.path.abspath("")
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
-from mrcnn import utils
+from mrcnn import utils, model
 from mrcnn import model as modellib
 from mrcnn import visualize
 
@@ -35,8 +35,9 @@ RESULTS_DIR = os.path.join(ROOT_DIR, "results")
 VAL_IMAGE_IDS =[]
 
 ############################################################
-#  Configurations
-############################################################
+#  Configurations-- just copied for now
+#############################################################
+
 
 
 
@@ -45,7 +46,159 @@ VAL_IMAGE_IDS =[]
 ############################################################
 
 class FindNote(utils.Dataset)
-    def load_notes(self, dataset_dir, subset)
+    def load_notes(self, dataset_dir, subset):
         self.add_class("note", 1, "note")
 
-    def
+        # Which subset?
+        # "val": use hard-coded list above
+        # "train": use data from stage1_train minus the hard-coded list above
+        # else: use the data from the specified sub-directory
+        # Add images
+        assert subset in ["train", "val", "stage1_train", "stage1_test", "stage2_test"]
+        subset_dir = "stage1_train" if subset in ["train", "val"] else subset
+        dataset_dir = os.path.join(dataset_dir, subset_dir)
+        if subset == "val":
+            image_ids = VAL_IMAGE_IDS
+        else:
+            # Get image ids from directory names
+            image_ids = next(os.walk(dataset_dir))[1]
+            if subset == "train":
+                image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
+
+        # Add images
+        for image_id in image_ids:
+            self.add_image(
+                "nucleus",
+                image_id=image_id,
+                path=os.path.join(dataset_dir, image_id, "images/{}.png".format(image_id)))
+
+    def load_mask(self, image_id):
+
+
+############################################################
+#  Training
+############################################################
+
+
+
+############################################################
+#  RLE Encoding
+############################################################
+
+
+
+############################################################
+#  Detection
+############################################################
+
+
+
+############################################################
+#  Command Line
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Mask R-CNN for nuclei counting and segmentation')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'detect'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/dataset/",
+                        help='Root directory of the dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--subset', required=False,
+                        metavar="Dataset sub-directory",
+                        help="Subset of dataset to run prediction on")
+    args = parser.parse_args()
+
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+    elif args.command == "detect":
+        assert args.subset, "Provide --subset to run prediction on"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    if args.subset:
+        print("Subset: ", args.subset)
+    print("Logs: ", args.logs)
+
+# Configurations 빈공간
+
+    if args.command == "train":
+        config =
+
+    elif args.command == "detect":
+        config =
+
+    config.display()
+
+
+    # Create model
+
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model, args.dataset, args.subset)
+    elif args.command == "detect":
+        detect(model, args.dataset, args.subset)
+    else:
+        print("'{}' is not recognized. ""Use 'train' or 'detect'".format(args.command))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
